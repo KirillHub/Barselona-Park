c764cec5f58a392c412931fdf1d1fc2e7defeb91
bi-zi <thebizi15@gmail.com>
2023-02-22 18:30:08 +0300

    fix for vercel

--- src/components/category page/category interaction/index.tsx
+++ src/components/category page/category interaction/index.tsx
@@ -182,7 +182,7 @@ export const CategoryInteraction = () => {
 
   useEffect(() => {
     firstRender();
-  }, []);
+  });
 
   return (
     <>
--- src/components/category page/category interaction/options/guests nights/index.tsx
+++ src/components/category page/category interaction/options/guests nights/index.tsx
@@ -4,40 +4,20 @@ import { More } from '../../../../../svg';
 import styles from '../style.module.scss';
 
 export const GuestsNights = () => {
-
-  const guests = useStore((state) => state.guests);
-
-  const plusGuests = useStore((state) => state.plusGuests);
-  const minusGuests = useStore((state) => state.minusGuests);
-
-
-  const onClickGuests = (action: string) => {
-    if ((guests === 1 && action === 'minus') || (guests === 8 && action === 'plus')) {
-      return;
-    } else if (action === 'plus') {
-      plusGuests();
-    } else {
-      minusGuests();
-    }
-  };
-
   return (
     <>
       <div className={styles.box}>
         <p className={styles.boxTitle}>Люди</p>
 
         <div className={styles.boxDate}>
-          <span className={styles.boxDateNumber}>{guests}</span>
+          <span className={styles.boxDateNumber}>{1}</span>
 
           <div>
-            <div
-              className={styles.optionsIcon + ' ' + styles.IconWrap}
-              onClick={() => onClickGuests('plus')}
-            >
+            <div className={styles.optionsIcon + ' ' + styles.IconWrap} onClick={() => ''}>
               <More />
             </div>
 
-            <div className={styles.optionsIcon} onClick={() => onClickGuests('minus')}>
+            <div className={styles.optionsIcon} onClick={() => ''}>
               <More />
             </div>
           </div>
@@ -45,9 +25,7 @@ export const GuestsNights = () => {
       </div>
 
       <div className={styles.box}>
-        <p className={styles.boxTitle}>
-          Ночей
-        </p>
+        <p className={styles.boxTitle}>Ночей</p>
         <div className={styles.boxDate}>
           <span className={styles.boxNights}>{365}</span>
         </div>
